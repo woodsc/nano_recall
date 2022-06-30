e5f332db719630c53795de7ace8687626d7f39d2
Conan Woods <gitlab@woodsc.ca>
2022-06-30 07:54:31 -0700

    Added better OS detection in the alignment code.

--- lib/alignment/_alignment.rb
+++ lib/alignment/_alignment.rb
@@ -1,8 +1,8 @@
 load_dir = File.dirname(__FILE__)
 
 #puts RUBY_PLATFORM
+if(RUBY_PLATFORM =~ /mswin|(win|w)32|cygwin|mingw$/)
 
-if(RUBY_PLATFORM =~ /(win|w)32$/ or RUBY_PLATFORM =~ /cygwin$/ )
 	if(RUBY_VERSION =~ /^2\.1/)
 		require "#{load_dir}/alignment.windows.r21.so"
 	elsif(RUBY_VERSION =~ /^2\.2/)
@@ -32,6 +32,7 @@ elsif(RUBY_PLATFORM =~ /x86_64-linux/)
 elsif(RUBY_PLATFORM =~ /i686-darwin10/)
     require "#{load_dir}/alignment.macosx.so"
 else
+	put "Unknown platform #{RUBY_PLATFORM}, defaulting to linux"
   if(RUBY_VERSION =~ /^2\.[23456789]/)
     require "#{load_dir}/alignment.linux32.r2.so"
   elsif(RUBY_VERSION =~ /^3\.[123456789]/)
