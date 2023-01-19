0a161d757a2228ddb888a0ce26d559621198cfc9
Conan Woods <gitlab@woodsc.ca>
2023-01-19 10:08:25 -0800

    Fix for changed platform strings on ruby 3.1 and on.

--- lib/alignment/_alignment.rb
+++ lib/alignment/_alignment.rb
@@ -2,11 +2,13 @@ load_dir = File.dirname(__FILE__)
 
 #puts RUBY_PLATFORM
 
-if(RUBY_PLATFORM =~ /mswin|(win|w)32|cygwin|mingw$/)
+if(RUBY_PLATFORM =~ /mswin|(win|w)32|cygwin|mingw/)
 	if(RUBY_VERSION =~ /^2\.1/)
 		require "#{load_dir}/alignment.windows.r21.so"
 	elsif(RUBY_VERSION =~ /^2\.2/)
 		require "#{load_dir}/alignment.windows.r22.so"
+	elsif(RUBY_PLATFORM =~ /ucrt/)
+		require 'alignment_ext'
 	else
 		require 'alignment_ext'
 	end
@@ -32,7 +34,7 @@ elsif(RUBY_PLATFORM =~ /x86_64-linux/)
 elsif(RUBY_PLATFORM =~ /i686-darwin10/)
     require "#{load_dir}/alignment.macosx.so"
 else
-	put "Unknown platform #{RUBY_PLATFORM}, defaulting to linux"
+	puts "Unknown platform #{RUBY_PLATFORM}, defaulting to linux"
   if(RUBY_VERSION =~ /^2\.[23456789]/)
     require "#{load_dir}/alignment.linux32.r2.so"
   elsif(RUBY_VERSION =~ /^3\.[123456789]/)
